f581bd5fe661fd4a0a809b6d4b66537bdc76907c
QWERTYUIOP098765ASDFGHJKL <samarkhanallen@gmail.com>
2025-03-26 22:19:03 +0530

    Update fixme.py by samar

--- fixme.py
+++ fixme.py
@@ -1,13 +1,19 @@
 def fibonacci_iterative(n):
     if n <= 0:
         return 0
-    elif n == 2: #Fix me
+    elif n == 1:  # Fixed by Samar
+        return 0
+    elif n == 2:  # Fixed by Samar
         return 1
     
     a, b = 0, 1
-    for _ in range(2, n+1):
+    for _ in range(2, n):  # Fixed by Samar
         a, b = b, a + b
- return b # Fix me.
+    return b  # Fixed by Samar
 
 n = 10
 print(f"The {n}th Fibonacci number is: {fibonacci_iterative(n)}")
+
+
+
+#edited by samar
